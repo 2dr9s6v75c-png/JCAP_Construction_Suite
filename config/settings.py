09426d2d5b8631288709e0bc8dfe102e9d37cf53
John Louies Canaria <johnlouiescanaria12@gmail.com>
2026-08-12 19:52:02 +0800

    Set Phase 1 UAT version to 0.9.0

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 APP_NAME = os.getenv("APP_NAME", "JCAP Construction Suite")
-APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
+APP_VERSION = os.getenv("APP_VERSION", "0.9.0")
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
